383cd8c54f117de687c7c95165e92595fb3ca0b0
Naufal F <naufalfirman098@gmail.com>
2025-04-10 04:39:58 +0700

    fix error in deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { api, HydrateClient } from "~/trpc/server";
-
 export default async function Home() {
-  void api.post.getLatest.prefetch();
-
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <h1>Please wait</h1>
-      </main>
-    </HydrateClient>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <h1>Please wait</h1>
+    </main>
   );
 }
